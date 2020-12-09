0a9aac6343f4455b2a7d013d24d6e770344a3d26
Rohan Yadav <rohany@cs.stanford.edu>
2020-12-09 14:07:01 -0800

    cli: update docstrings on Command struct
    This gives the CLI itself nice help messages in the command line.
    
    Fixes #10.

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,39 +1,69 @@
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
-/// Commands is an enum representing the possible command line invocations.
-/// Currently, vault supports the following commands:
-///   * vault register <experiment_name>
-///   * vault store <experiment_name> <instance_directory>
-///   * vault add-meta <instance_directory> <key> <value>
-///   * vault list-meta <instance_directory>
-///   * vault get-latest <experiment_name>
-///   * vault query <experiment_name> <query>
+/// Vault is a tool for managing and organizing experimental data.
+///
+/// A standard Vault workflow will create an experiment using `vault register`,
+/// generate instances of the experiment with metadata using `vault add-meta`,
+/// and then store the data in Vault using `vault store`.
+//
+// Commands is an enum representing the possible command line invocations.
+// Currently, vault supports the following commands:
+//   * vault register <experiment_name>
+//   * vault store <experiment_name> <instance_directory>
+//   * vault add-meta <instance_directory> [kv <key> <value> | json <json dict>]
+//   * vault list-meta <instance_directory>
+//   * vault get-latest <experiment_name>
+//   * vault query <experiment_name> <query>
 //
 // All commands here must be included in integration tests in tests/cli.rs.
+// Additionally, all commands must "doc" comments (///), as these are included
+// in the generated help messages visible to end users.
 pub enum Commands {
+    /// Register a new experiment.
+    ///
+    /// Instances of the created experiment can be added using `vault store`.
     Register {
+        /// Name of the experiment to register.
         experiment: String,
     },
+    /// Store an instance of an experiment in Vault.
     Store {
+        /// Name of the target experiment.
         experiment: String,
+        /// Path to an experiment instance directory.
         instance: String,
     },
-    // This could be a bigger "meta" command that people can use to adjust the meta
-    // on their experiment.
+    // TODO (rohany): This could be a bigger "meta" command that people can use
+    // to adjust the meta on their experiment.
+    /// Add metadata to an experiment instance.
+    ///
+    /// add-meta can only be used to attach metadata to an experiment instance before it
+    /// has been added to Vault.
     AddMeta {
+        /// Path to an experiment instance directory.
         directory: String,
         #[structopt(subcommand)]
+        /// The kind of metadata to add.
         meta: AddMetaKind,
     },
+    /// List metadata attached to an experiment instance.
     ListMeta {
+        /// Path to an experiment instance directory.
         directory: String,
     },
+    /// Get the latest instance of an experiment.
+    ///
+    /// get-latest returns a directory containing the instance.
     GetLatest {
+        /// Name of the target experiment.
         experiment: String,
     },
+    /// Query an experiment for instances satisfying some criteria.
     Query {
+        /// Name of the target experiment.
         experiment: String,
+        /// Query in the Vault Query DSL (detailed in [TODO (rohany)]).
         query: String,
     },
 }
@@ -41,7 +71,10 @@ pub enum Commands {
 #[derive(StructOpt, Debug)]
 // AddMetaKind represents a kind of metadata to add to an instance. This data
 // can either be a single key-value pair, or a JSON set of key-value pairs.
+/// The kind of metadata to attach -- either a key-value pair, or a JSON dictionary.
 pub enum AddMetaKind {
+    /// Add a key-value pair.
     KV { key: String, value: String },
+    /// Add all of the key-value pairs in a JSON dictionary.
     JSON { json: String },
 }
